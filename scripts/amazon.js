7af331d227a1f5687e3bade4e9be70491f4dc257
AnastasiyaKuzhba <anastasiya.kuzhba@gmail.com>
2024-04-03 15:58:37 +0200

    Quantity selector that allows to choose more than 1 product (from 2 till 10)

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -27,7 +27,7 @@ products.forEach((product) => {
       </div>
 
       <div class="product-quantity-container">
-        <select>
+        <select class="js-quantity-selector-${product.id}">
           <option selected value="1">1</option>
           <option value="2">2</option>
           <option value="3">3</option>
@@ -69,12 +69,17 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
     }
   });
 
+  const quantitySelector = document.querySelector(`.js-quantity-selector-${productId}`);
+
+    const quantity = quantitySelector.value;
+    const number = Number(quantity);
+
   if (matchingItem) {
-    matchingItem.quantity += 1;
+    matchingItem.quantity += number;
   } else {
     cart.push({
       productId: productId,
-      quantity: 1
+      quantity: number
     });
   }
 
@@ -85,6 +90,6 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
 
   document.querySelector('.js-cart-quantity')
   .innerHTML = cartQuantity;
-  });
+  }); 
 });
 
